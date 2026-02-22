7e1b88cb3b5463ce85e8c37e232a51e1c35d26bf
Conceal34 <vinnerhooda@gmail.com>
2026-02-22 18:54:35 +0530

    basics ready of both services they work independently

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,27 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+allData = []
+
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({"message": "Welcome to the BACKEND!"})
+
+@app.route('/insertData', methods=['GET', 'POST'])
+def insertData():
+    data = {
+        'id': 1,
+        'name': 'John Doe',
+        'email': 'akujsbghdkjxc'
+    }
+    allData.append(data)
+    
+    return jsonify({'message': 'Data inserted successfully'}), 200
+
+
+@app.route('/getData', methods=['GET'])
+def getData():
+    return jsonify(allData), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,48 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import ejs from 'ejs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+const BACKEND_URL = process.env.BACKEND_URL || 'http://0.0.0.0:5000';
+
 app.get('/', (req, res) => {
-    res.send('Hello from the frontend!');
+    res.render('index')
+});
+
+app.get('/form', async (req, res) => {
+    res.render('form')
+})
+
+app.post('/submit', async (req, res) => {
+    // redirect to the backend service to process the form data
+    // try {
+    //     const response = await fetch(`${BACKEND_URL}/insertData`, {
+    //         method: 'POST',
+    //         headers: { 'Content-Type': 'application/json' },
+    //         body: JSON.stringify(req.body)
+    //     });
+    //     if (!response.ok) {
+    //         throw new Error('Failed to process form data');
+    //     }
+    // }
+    // catch (error) {
+    //     console.error('Error processing form data:', error);
+    //     return res.render('form_failure');
+    // }
+
+    res.render('form_success', { name: req.body.name, email: req.body.email });
 });
 
 app.listen(3000, () => {
